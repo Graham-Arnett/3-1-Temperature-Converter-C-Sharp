74041d6f30d539d23a9fd63fd715c2d9f404ca5b
Graham Arnett <GrahamKentArnett@outlook.com>
2024-10-16 15:52:28 -0400

    init commit

---
+++ TempConv/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TempConv
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the temperature converter app");
+            String choice;
+            do
+            {
+                Console.Write("Enter a temperature in fahrenheit degrees: ");
+
+                double fahrenheit = double.Parse(Console.ReadLine());
+
+                double celsius = (fahrenheit - 32) * 5 / 9;
+
+                double kelvin = celsius + 273.15;
+
+                //to limit the decimal places to two
+                double roundedCelsius = Math.Round(celsius, 2);
+                double roundedKelvin = Math.Round(kelvin, 2);
+
+                Console.WriteLine($"Your temperature in celsius: {roundedCelsius}");
+                Console.WriteLine($"Your temperature in kelvin: {roundedKelvin}");
+
+                Console.Write("Would you like to go again? (y/n): ");
+                choice = Console.ReadLine().ToLower().Trim();
+            }while(choice == "y");
+
+        }
+    }
+}
